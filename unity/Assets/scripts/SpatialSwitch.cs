a498639a475cfe1a3b8fe8c6238cc49ea112d987
Tatsuya FUJIWARA <fjtatsuya@gmail.com>
2017-12-14 13:25:14 +0900

    add: timeline上でanimationする物体が発音するsceneを追加

---
+++ unity/Assets/scripts/SpatialSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatialSwitch : MonoBehaviour {
+    AudioSource source;
+    void Awake()
+    {
+        source = transform.GetComponent<AudioSource>();
+    }
+    void OnEnable()
+    {
+        source.spatialBlend = 1;
+    }
+
+    void OnDisable()
+    {
+        source.spatialBlend = 0;
+    }
+}
